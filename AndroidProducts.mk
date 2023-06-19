33a641cc6da9ea21d3e9b78ab5cc469d5b4902ba
Niranjan BR <niranjankannan2003@gmail.com>
2023-06-19 00:42:42 +0000

    sweet: Initial bringup for Pixys OS - Removed unused overalys - Fix bootanimation resolution
    Signed-off-by: Niranjan BR <niranjankannan2003@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/voltage_sweet.mk
+    $(LOCAL_DIR)/pixys_sweet.mk
 
 COMMON_LUNCH_CHOICES := \
-    voltage_sweet-user \
-    voltage_sweet-userdebug \
-    voltage_sweet-eng
+    pixys_sweet-user \
+    pixys_sweet-userdebug \
+    pixys_sweet-eng
