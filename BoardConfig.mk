3059a760c120bc969ab4d9622cc79ca51e54c438
Niranjan BR <niranjankannan2003@gmail.com>
2024-08-02 14:01:28 +0530

    sweet: move props to seperate section
    Signed-off-by: Niranjan BR <niranjankannan2003@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -193,9 +193,9 @@ BOARD_USES_METADATA_PARTITION := true
 TARGET_BOARD_PLATFORM := sm6150
 
 # Properties
-TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
-TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
-TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+TARGET_ODM_PROP += $(DEVICE_PATH)/properties/odm.prop
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/properties/system.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/properties/vendor.prop
 
 # QCOM
 BOARD_USES_QCOM_HARDWARE := true
